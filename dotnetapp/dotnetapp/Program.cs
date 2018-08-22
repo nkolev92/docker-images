5a0a9f92adbe3a5a3117c7d6584e681e02a21527
Nikolche Kolev <nikolev@microsoft.com>
2018-08-21 21:38:00 -0700

    work

--- dotnetapp/dotnetapp/Program.cs
+++ dotnetapp/dotnetapp/Program.cs
@@ -1,19 +1,21 @@
-using System;
 using System.Runtime.InteropServices;
-using System.IO;
-using System.Text;
 using System.Globalization;
 using static System.Console;
 
 public static class Program
 {
-    public static void Main(string[] args) 
+    public static void Main(string[] args)
     {
-          WriteLine("**Environment**");
-          WriteLine($"Platform: .NET Core");
-          WriteLine($"OS: {RuntimeInformation.OSDescription}");
-          WriteLine($"CurrentCulture: `{CultureInfo.CurrentCulture}`");
-          WriteLine($"CurrentCulture.Name: `{CultureInfo.CurrentCulture.Name}`");
-          WriteLine();
+        WriteLine("**Environment**");
+        WriteLine($"Platform: .NET Core");
+        WriteLine($"OS: {RuntimeInformation.OSDescription}");
+        WriteLine($"CurrentCulture: `{CultureInfo.CurrentCulture}`");
+        WriteLine($"CurrentCulture.Name: `{CultureInfo.CurrentCulture.Name}`");
+        WriteLine($"CurrentCulture.DisplayName: `{CultureInfo.CurrentCulture.DisplayName}`");
+        WriteLine($"CurrentCulture.IsNeutralCulture: `{CultureInfo.CurrentCulture.IsNeutralCulture}`");
+        WriteLine($"CurrentCulture.NativeName: `{CultureInfo.CurrentCulture.NativeName}`");
+        WriteLine($"CurrentCulture.ThreeLetterISOLanguageName: `{CultureInfo.CurrentCulture.ThreeLetterISOLanguageName}`");
+        WriteLine($"Is the culture invariant?: `{CultureInfo.CurrentCulture.Equals(CultureInfo.InvariantCulture)}");
+        WriteLine();
     }
 }
